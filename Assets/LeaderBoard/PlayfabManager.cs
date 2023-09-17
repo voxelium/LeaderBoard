c15ab89cbd7c1fd4eb693316e110dd2e35ef51f5
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2023-09-17 15:16:45 +0400

    Leaderboard around player position

--- Assets/LeaderBoard/PlayfabManager.cs
+++ Assets/LeaderBoard/PlayfabManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
 using TMPro;
+using System;
 
 public class PlayfabManager : MonoBehaviour
 {
@@ -15,15 +16,18 @@ public class PlayfabManager : MonoBehaviour
     [SerializeField] private GameObject nameError;
     [SerializeField] private TMP_InputField nameInput;
     
-
     [Header("LeaderBoard")]
     [SerializeField] private GameObject leadRow;
     [SerializeField] private Transform leadRowsParent;
 
+
     [Header("Input datas")]
     [SerializeField] private TextMeshProUGUI clicksText;
     [SerializeField] private TextMeshProUGUI leadersText;
-    [SerializeField] private string tempName;
+    [SerializeField] private string tempCustomID;
+
+    private string loggedInPlayfabID;
+
     private int clicksNum = 0;
 
     private void Start()
@@ -36,8 +40,8 @@ public class PlayfabManager : MonoBehaviour
     {
         var request = new LoginWithCustomIDRequest
         {
-            TitleId = tempName,
             //CustomId = SystemInfo.deviceUniqueIdentifier,
+            CustomId = tempCustomID,
             CreateAccount = true,
             InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
             {
@@ -52,20 +56,28 @@ public class PlayfabManager : MonoBehaviour
 
     private void OnLoginSuccess(LoginResult result)
     {
-        Debug.Log("Successfull login/account create");
+        Debug.Log("Successfull login/account created");
+        loggedInPlayfabID = result.PlayFabId;
+
+        GetLeaderboard();
+
         string name = null;
+
         if (result.InfoResultPayload.PlayerProfile != null)
         {
             name = result.InfoResultPayload.PlayerProfile.DisplayName;
+
+            Debug.Log(name);
         }
 
         if (name == null)
         {
             nameWindow.SetActive(true);
         }
-        else
-            leaderboardWindow.SetActive(true);
-        
+        //else
+        //    leaderboardWindow.SetActive(true);
+
+
     }
 
     public void SubmitNameButton()
@@ -80,7 +92,9 @@ public class PlayfabManager : MonoBehaviour
 
     private void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
     {
-        leaderboardWindow.SetActive(true);
+        //leaderboardWindow.SetActive(true);
+        nameWindow.SetActive(false);
+        GetLeaderboard();
     }
 
 
@@ -117,17 +131,22 @@ public class PlayfabManager : MonoBehaviour
     private void OnLeaderBoardUpdate(UpdatePlayerStatisticsResult result)
     {
         Debug.Log("Successfull leaderboard sent");
+        //Пробовал здесь вызывать метод GetLeaderboard
+        //Несмотря на то, что он вызывается, но реально данные не приходят с первого раза.
+        //Приходится вызывать из повторно
+        //Возможно должно пройти время между запросами Sent и Get
     }
 
 
 
     public void GetLeaderboard()
     {
+        Debug.Log("Getting Leaderboard");
         var request = new GetLeaderboardRequest
         {
             StatisticName = "BestScore",
             StartPosition = 0,
-            MaxResultsCount = 500
+            MaxResultsCount = 100
         };
         PlayFabClientAPI.GetLeaderboard(request, OnLeaderBoardGet, OnError);
     }
@@ -135,9 +154,11 @@ public class PlayfabManager : MonoBehaviour
 
     private void OnLeaderBoardGet(GetLeaderboardResult result)
     {
+        ClearLeaderBoardScreen();
+
         foreach (var item in result.Leaderboard)
         {
-            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue );
+            //Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue );
 
             GameObject newGo = Instantiate(leadRow,leadRowsParent);
             TextMeshProUGUI[] texts = newGo.GetComponentsInChildren<TextMeshProUGUI>();
@@ -146,8 +167,58 @@ public class PlayfabManager : MonoBehaviour
             texts[1].text = item.DisplayName;
             texts[2].text = item.StatValue.ToString();
 
+            if (item.PlayFabId == loggedInPlayfabID)
+            {
+                texts[0].color = Color.yellow;
+                texts[1].color = Color.yellow;
+                texts[2].color = Color.yellow;
+            }
         }
-        
+    }
+
+
+    public void GetLeaderBoardAroundPlayer()
+    {
+        var request = new GetLeaderboardAroundPlayerRequest
+        {
+            StatisticName = "BestScore",
+            MaxResultsCount = 5
+        };
+        PlayFabClientAPI.GetLeaderboardAroundPlayer(request, OnLeaderBoardAroundPlayerGet, OnError);
+    }
+
+
+    private void OnLeaderBoardAroundPlayerGet(GetLeaderboardAroundPlayerResult result)
+    {
+        ClearLeaderBoardScreen();
+
+        foreach (var item in result.Leaderboard)
+        {
+            //Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue );
+
+            GameObject newGo = Instantiate(leadRow, leadRowsParent);
+            TextMeshProUGUI[] texts = newGo.GetComponentsInChildren<TextMeshProUGUI>();
+
+            texts[0].text = (item.Position + 1).ToString();
+            texts[1].text = item.DisplayName;
+            texts[2].text = item.StatValue.ToString();
+
+            if (item.PlayFabId == loggedInPlayfabID)
+            {
+                texts[0].color = Color.yellow;
+                texts[1].color = Color.yellow;
+                texts[2].color = Color.yellow;
+            }
+        }
+    }
+
+
+    private void ClearLeaderBoardScreen()
+    {
+        foreach (Transform child in leadRowsParent)
+        {
+            Destroy(child.gameObject);
+        }
     }
 
 
@@ -156,5 +227,13 @@ public class PlayfabManager : MonoBehaviour
         clicksNum++;
         clicksText.text = clicksNum.ToString();
     }
+
+
+    public void ResetClicks()
+    {
+        clicksNum = 0;
+        clicksText.text = clicksNum.ToString();
+    }
+
 
 }
